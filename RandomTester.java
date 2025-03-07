34b909eeec5ee2163b57073274f8d64a80f729af
Fatima Kanaev <kanaevfatima@gmail.com>
2025-03-07 15:27:55 -0500

    Question 15 as comment

--- RandomTester.java
+++ RandomTester.java
@@ -6,6 +6,14 @@ import java.util.Random;
  * @author (your name)
  * @version (a version number or a date)
  */
+
+/*
+ * Question 15
+ * It will generate a random number from 0 to 99
+ * if you want a number between 1 and 100, write rand.nextInt(100)+1
+ * 
+ */
+
 public class RandomTester
 {
     private Random r = new Random();

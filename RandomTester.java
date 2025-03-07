6e3b24ec1f46b532e22bc49a8f37cb80491db922
Fatima Kanaev <kanaevfatima@gmail.com>
2025-03-07 15:25:39 -0500

    Question 14

---
+++ RandomTester.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+/**
+ * Write a description of class RandomTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RandomTester
+{
+    private Random r = new Random();
+    
+    public void printOneRandom(){
+        System.out.println(" new integer:"+ r.nextInt(500));
+    }
+    
+    public void printMultiRandom(int howMany){
+        for (int i = 0; i < howMany; i++){
+            System.out.println(" new integer:"+ r.nextInt(500));
+        }
+    }
+}

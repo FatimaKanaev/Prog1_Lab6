423802782f10b7db5fbc826461d5452e2fe95785
Fatima Kanaev <kanaevfatima@gmail.com>
2025-03-09 21:18:17 -0400

    Question 28 to 32

--- MapTester.java
+++ MapTester.java
@@ -6,6 +6,24 @@ import java.util.HashMap;
  * @author (your name)
  * @version (a version number or a date)
  */
+
+/*
+ * Question 28
+ * The existing value is replaced with a new value
+ * 
+ * Question 29
+ * Both keys and their associated values are stored seperately
+ * 
+ * Question 30
+ * we use a containsKey() method : System.out.println(map.containsKey("Fatima"));
+ * 
+ * Question 31
+ * The get() method returns as ''null''
+ * 
+ * Question 32
+ * We have to use the keySet() method to print out all the keys stored in a map
+ */
+
 public class MapTester
 {
     private HashMap<String,String> phoneBook = new HashMap<>();
